b12c2123d1ce1377d2ebbfc7048747ab2d01cfbb
treemcgee42 <varun.malladi@gmail.com>
2024-05-14 11:05:38 -0400

    Ace window
    Adds the package ace-window as well as a useful prefix command.
    Inspired by karthinks.

--- init.el
+++ init.el
@@ -113,6 +113,26 @@
   (global-set-key (kbd "C-;") 'avy-goto-char-timer)
   (global-set-key (kbd "C-c ;") 'avy-goto-char-timer))
 
+(defun ace-window-prefix ()
+  "Use `ace-window' to display the buffer of the next command.
+The next buffer is the buffer displayed by the next command invoked
+immediately after this command (ignoring reading from the minibuffer).
+Creates a new window before displaying the buffer.
+When `switch-to-buffer-obey-display-actions' is non-nil,
+`switch-to-buffer' commands are also supported."
+  (interactive)
+  (display-buffer-override-next-command
+   (lambda (buffer _)
+     (let (window type)
+       (setq
+        window (aw-select (propertize " ACE" 'face 'mode-line-highlight))
+        type 'reuse)
+       (cons window type)))
+   nil "[ace-window]")
+  (message "Use `ace-window' to display next command buffer..."))
+(setq switch-to-buffer-obey-display-actions t)
+(keymap-global-set "C-x 4 o" 'ace-window-prefix)
+
 (defun tm42-smart-beginning-of-line ()
   "Move point to first beginning of line or non-whitespace character.
 If the point is already at the beginning of the line, move to the
