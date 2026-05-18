bf562dc7920e2c0350a1824603315364e6c953d5
Varun Malladi <varun.malladi@gmail.com>
2026-05-18 08:06:05 -0400

    slightly lighten whitespace in midnight-blue

--- emacs/midnight-blue-theme.el
+++ emacs/midnight-blue-theme.el
@@ -144,7 +144,7 @@
  '(ediff-odd-diff-C ((t (:background "#0a0a0a" :foreground unspecified))) t)
 
  ;; --- Whitespace
- '(whitespace-space ((t :foreground "#1a1a1a"))))
+ '(whitespace-space ((t :foreground "#303030"))))
 
 (provide-theme 'midnight-blue)
 
